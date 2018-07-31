b35414f7fdc96d5321956f80a624bc4360664d1d
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-31 00:38:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,1 @@
-def reformat_languages(languages)
-  final_hash = {}
-  languages.each do |style, langs|
-  	langs.each do |lang, type|
-  		final_hash[lang] = type
-  		final_hash[lang][:style] = []
-  		final_hash[lang][:style] << style
-  		final_hash[lang][:style] << :oo if lang == :javascript
-  	end
-  end
-  final_hash
-end
+
